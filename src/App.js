7802bd100ee36c784305fefb6fe89147b38cb1af
Siddarth-Sathyamoorthy <siddarrthsathi@gmail.com>
2023-11-30 12:47:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import VideoPlayer from './VideoPlayer';
 
 const App = () => {
-  const videoSource = 'https://pixeldrain.com/api/file/CkZL6pD1?download';
+  const videoSource = 'https://pixeldrain.com/api/file/LgcWmv4Z?download';
   //const videoSource = 'https://6xc5ppe9ei.e6278d.com/user1342/c751bab1939a2b83020565e1ac242896/EP.1.v1.1080p.mp4?token=9KGCbXs-Wgeq7zz7asQM4A&expires=1701268932&id=3518';
 
   return (
